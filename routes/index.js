1823fb3d1df36427c613f8b16c75069efc78d867
geoffroy goirand <geoffroy.goirand@gmail.com>
2022-06-30 21:26:03 +0200

    trips2

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var connect = require('../models/connect')
 var journeyModel = require('../models/journey')
+var UserModel = require('../models/users')
 
 var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
 var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
