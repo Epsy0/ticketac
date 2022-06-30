6f3a3c938a1b8b5634c98f1139219d304307b45a
geoffroy goirand <geoffroy.goirand@gmail.com>
2022-06-30 15:00:18 +0200

    databaseconnect4

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var connect = require('../models/connect')
 var UserModel = require('../models/users')
-var journeyModel = require('../models/journeys')
+var journeyModel = require('../models/journey')
 
 var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
 var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
